26e4bdb8d41a96821e1d727f02692b6d0477a528
elon <f1570053668@163.com>
2021-05-09 22:08:12 +0800

    字符串-匹配

---
+++ 28.实现-str-str.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * KMP算法 O(m+n)
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        for (int i = 0; i + m <= n; i++) {
+            bool flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
