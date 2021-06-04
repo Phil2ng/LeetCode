0569a9e389eeb4ebb44c3566686a8d00a229f3f4
elon <f1570053668@163.com>
2021-06-04 12:42:26 +0800

    遍历/递归

---
+++ 剑指 Offer 06. 从尾到头打印链表.py
@@ -0,0 +1,13 @@
+# 遍历/递归
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if head is None:
+            return []
+        return self.reversePrint(head.next) + [head.val]
