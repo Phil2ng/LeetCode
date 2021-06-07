58a0ddfcadfa0a0ed15d74a1fdaf2a0a1533898f
elon <f1570053668@163.com>
2021-06-07 21:24:59 +0800

    二分查找

---
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字.py
@@ -0,0 +1,12 @@
+# 二分查找
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid] == mid:
+                l = mid+1
+            else:
+                r = mid-1
+        return l if nums[l] != l else l+1
