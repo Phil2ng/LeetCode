e481e5828909b70832ba92c20b3ab4904d87a4ad
elon <f1570053668@163.com>
2021-08-09 17:01:11 +0800

    最小堆/动态规划

--- 313.超级丑数.cpp
+++ 313.超级丑数.cpp
@@ -1,6 +1,8 @@
 /*
  * @lc app=leetcode.cn id=313 lang=cpp
  *
+ * 最小堆,或者动态规划
+ * 
  * [313] 超级丑数
  */
 
@@ -29,6 +31,34 @@ public:
             }
         }
         return (int)pq.top();
+    }
+};
+class Solution
+{
+public:
+    int nthSuperUglyNumber(int n, vector<int> &primes)
+    {
+        int m = primes.size();
+        vector<int> ans, idx(m, 0);
+        ans.push_back(1);
+        while (--n)
+        {
+            int minNum = INT_MAX, num;
+            for (int i = 0; i < m; i++)
+            {
+                num = primes[i] * ans[idx[i]];
+                if (minNum > num)
+                    minNum = num;
+            }
+            ans.push_back(minNum);
+            for (int i = 0; i < m; i++)
+            {
+                num = primes[i] * ans[idx[i]];
+                if (minNum == num)
+                    idx[i]++;
+            }
+        }
+        return ans.back();
     }
 };
 // @lc code=end
