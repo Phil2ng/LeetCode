7bc2dc444d821f2f834623d7676885e7ef2d66bd
elon <f1570053668@163.com>
2021-10-04 10:28:37 +0800

    模拟遍历

---
+++ 482.密钥格式化.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=482 lang=cpp
+ *
+ * 模拟遍历
+ * 
+ * [482] 密钥格式化
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string licenseKeyFormatting(string s, int k)
+    {
+        string ans;
+        int cnt = 0;
+
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            if (s[i] != '-')
+            {
+                ans.push_back(toupper(s[i]));
+                cnt++;
+                if (cnt % k == 0)
+                {
+                    ans.push_back('-');
+                }
+            }
+        }
+        if (ans.size() > 0 && ans.back() == '-')
+        {
+            ans.pop_back();
+        }
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+// @lc code=end
+
