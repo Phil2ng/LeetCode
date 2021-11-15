ba51afd93aa753ce335eebd93cada14ab3f7fb7c
elon <f1570053668@163.com>
2021-11-15 20:05:58 +0800

    数学

---
+++ 319.灯泡开关.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * 数学
+ * 
+ * [319] 灯泡开关
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+// @lc code=end
+
