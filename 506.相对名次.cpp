cf7b8dc772727db9bf8e61beb999666a0ea1d397
elon <f1570053668@163.com>
2021-12-04 22:03:37 +0800

    排序

--- 506.相对名次.cpp
+++ 506.相对名次.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * @lc app=leetcode.cn id=506 lang=cpp
  *
@@ -28,35 +27,4 @@ public:
         return ans;
     }
 };
-=======
-/*
- * @lc app=leetcode.cn id=506 lang=cpp
- *
- * [506] 相对名次
- */
-
-// @lc code=start
-class Solution {
-public:
-    vector<string> findRelativeRanks(vector<int>& score) {
-        int n = score.size();
-        string desc[3] = {"Gold Medal", "Silver Medal", "Bronze Medal"};
-        vector<pair<int, int>> arr;
-
-        for (int i = 0; i < n; ++i) {
-            arr.emplace_back(make_pair(-score[i], i));
-        }
-        sort(arr.begin(), arr.end());
-        vector<string> ans(n);
-        for (int i = 0; i < n; ++i) {
-            if (i >= 3) {
-                ans[arr[i].second] = to_string(i + 1);
-            } else {
-                ans[arr[i].second] = desc[i];
-            }
-        }
-        return ans;
-    }
-};
->>>>>>> 69920c4353677f9f30bbf7109415dbee3f890af0
 // @lc code=end
