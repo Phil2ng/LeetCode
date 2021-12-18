3556cee917d3f8eaecca6293dd17f889a6819789
Phil <f1570053668@163.com>
2021-12-18 10:57:50 +0800

    遍历

---
+++ 419.甲板上的战舰.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=419 lang=python3
+#
+# [419] 甲板上的战舰
+#
+
+# @lc code=start
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        ans = 0
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X':
+                    if i > 0 and board[i - 1][j] == 'X':
+                        continue
+                    if j > 0 and board[i][j - 1] == 'X':
+                        continue
+                    ans += 1
+        return ans
+# @lc code=end
+
