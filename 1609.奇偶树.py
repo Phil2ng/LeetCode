0a6824ad1a76d9eb03008a26a051be6ffdec842e
Phil <f1570053668@163.com>
2021-12-25 09:29:00 +0800

    广度优先遍历

--- 1609.奇偶树.py
+++ 1609.奇偶树.py
@@ -15,5 +15,41 @@
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val%2==0:
+            return False
+        que = deque()
+        que.append(root)
+        t = 0
+
+        def less(a, b, t):
+            print(a,b,t)
+            if t % 2:
+                return a > b
+            return a < b
+
+        while que:
+            t += 1
+            n = len(que)
+            flag = False
+            for _ in range(n):
+                q = que.popleft()
+                print(q.val)
+                if q.left:
+                    if (q.left.val%2 ^ t%2) == 0:
+                        return False
+                    if flag:
+                        if not less(que[-1].val, q.left.val, t):
+                            return False
+                    que.append(q.left)
+                    flag = True
+                if q.right:
+                    if (q.right.val%2 ^ t%2) == 0:
+                        return False
+                    if flag:
+                        if not less(que[-1].val, q.right.val, t):
+                            return False
+                    que.append(q.right)
+                    flag = True
+        return True
 # @lc code=end
 
