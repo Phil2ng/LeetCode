ab8e878f252fdc241e6406c2faf8758b4b7321ac
Phil <f1570053668@163.com>
2022-01-03 18:16:07 +0800

    模拟遍历

---
+++ 1185.一周中的第几天.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1185 lang=python3
+#
+# [1185] 一周中的第几天
+#
+
+# @lc code=start
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        return datetime.date(year, month, day).strftime('%A')
+# @lc code=end
+
