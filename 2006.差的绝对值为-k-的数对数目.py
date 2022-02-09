319c3b8b8fbcc83d600085cbdb81913cfc696c8f
Phil <f1570053668@163.com>
2022-02-09 08:57:21 +0800

    遍历

---
+++ 2006.差的绝对值为-k-的数对数目.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=2006 lang=python3
+#
+# [2006] 差的绝对值为 K 的数对数目
+#
+
+# @lc code=start
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if abs(nums[i] - nums[j]) == k:
+                    ans += 1
+        return ans
+# @lc code=end
+
