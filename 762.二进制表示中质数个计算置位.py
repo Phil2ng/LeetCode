1c3e45c879aa4a9230fc4c8291a5498d975f9d17
Phil <f1570053668@163.com>
2022-04-05 22:25:12 +0800

    遍历

---
+++ 762.二进制表示中质数个计算置位.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=762 lang=python3
+#
+# [762] 二进制表示中质数个计算置位
+#
+
+
+# @lc code=start
+class Solution:
+    def isPrime(self, x: int) -> bool:
+        if x < 2:
+            return False
+        i = 2
+        while i * i <= x:
+            if x % i == 0:
+                return False
+            i += 1
+        return True
+
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        return sum(self.isPrime(x.bit_count()) for x in range(left, right + 1))
+
+
+# @lc code=end
