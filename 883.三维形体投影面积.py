14fe8d8d9bef867fbd8ecef4687ede989bdae659
Phil <f1570053668@163.com>
2022-04-27 09:50:25 +0800

    数学

---
+++ 883.三维形体投影面积.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=883 lang=python3
+#
+# [883] 三维形体投影面积
+#
+
+# @lc code=start
+class Solution:
+    def projectionArea(self, grid: List[List[int]]) -> int:
+        xyArea = sum(v > 0 for row in grid for v in row)
+        yzArea = sum(map(max, grid))
+        zxArea = sum(map(max, zip(*grid))) 
+        return xyArea + yzArea + zxArea
+# @lc code=end
+
