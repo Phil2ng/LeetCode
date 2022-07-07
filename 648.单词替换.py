3eff25d41015eadb5924475fb67494b383a1df1c
Phil <f1570053668@163.com>
2022-07-07 09:16:57 +0800

    字典树

---
+++ 648.单词替换.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=648 lang=python3
+#
+# [648] 单词替换
+#
+
+# @lc code=start
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = {}
+        for word in dictionary:
+            cur = trie
+            for c in word:
+                if c not in cur:
+                    cur[c] = {}
+                cur = cur[c]
+            cur['#'] = {}
+        words = sentence.split(' ')
+        for i, word in enumerate(words):
+            cur = trie
+            for j, c in enumerate(word):
+                if '#' in cur:
+                    words[i] = word[:j]
+                    break
+                if c not in cur:
+                    break
+                cur = cur[c]
+        return ' '.join(words)
+# @lc code=end
+
