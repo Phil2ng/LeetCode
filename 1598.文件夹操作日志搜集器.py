e8cd66829d98968d8298b145e94ef7082a35198a
Phil <f1570053668@163.com>
2022-09-09 14:51:56 +0800

    模拟

---
+++ 1598.文件夹操作日志搜集器.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=1598 lang=python3
+#
+# [1598] 文件夹操作日志搜集器
+#
+
+# @lc code=start
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0
+        for log in logs:
+            if log == "./":
+                continue
+            if log != "../":
+                depth += 1
+            elif depth:
+                depth -= 1
+        return depth
+# @lc code=end
+
