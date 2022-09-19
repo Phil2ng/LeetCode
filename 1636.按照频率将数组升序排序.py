2cb9fec1d4341f1f12effa85b299b2e9795e00c7
Phil <f1570053668@163.com>
2022-09-19 23:56:13 +0800

    模拟

---
+++ 1636.按照频率将数组升序排序.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=1636 lang=python3
+#
+# [1636] 按照频率将数组升序排序
+#
+
+# @lc code=start
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        nums.sort(key=lambda x: (cnt[x], -x))
+        return nums
+# @lc code=end
+
