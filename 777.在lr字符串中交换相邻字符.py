0d96828775307f330c0979438afa5bbcb4320efb
Phil <f1570053668@163.com>
2022-10-02 10:17:57 +0800

    规律

---
+++ 777.在lr字符串中交换相邻字符.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode.cn id=777 lang=python3
+#
+# [777] 在LR字符串中交换相邻字符
+#
+
+# @lc code=start
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        n = len(start)
+        i = j = 0
+        while i < n and j < n:
+            while i < n and start[i] == 'X':
+                i += 1
+            while j < n and end[j] == 'X':
+                j += 1
+            if i < n and j < n:
+                if start[i] != end[j]:
+                    return False
+                c = start[i]
+                if c == 'L' and i < j or c == 'R' and i > j:
+                    return False
+                i += 1
+                j += 1
+        while i < n:
+            if start[i] != 'X':
+                return False
+            i += 1
+        while j < n:
+            if end[j] != 'X':
+                return False
+            j += 1
+        return True
+# @lc code=end
+
